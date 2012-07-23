2bb562fe9b0c4a2ec809542e25644024b7300734
Aaron Khoo <aaron.khoo@geolopigs.com>
2012-07-23 10:49:27 -0700

    Removing validation for Post name for the moment.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,4 @@
 class Post < ActiveRecord::Base
-  validates :name, :presence => true
   validates :latitude, :presence => true
   validates :longitude, :presence => true
   validates :user_id, :presence => true
