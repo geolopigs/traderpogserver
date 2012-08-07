14013338c41e0e6ff1abde6ede32300ecaeccdc8
Aaron Khoo <aaron.khoo@geolopigs.com>
2012-08-06 23:40:28 -0700

    Fix CSRF warnings for JSON POST calls.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  skip_before_filter :verify_authenticity_token, :if => :json_request?
+
+  protected
+  def json_request?
+    request.format.json?
+  end
 end
