ff07a3b27a54d5f161c54b9b7f1bdce7508b0ef0
Aaron Khoo <aaron.khoo@geolopigs.com>
2012-08-08 15:43:04 -0700

    Fix a small bug with the user flyers API that was missing a "done" being returned.

--- app/helpers/user_flyers_helper.rb
+++ app/helpers/user_flyers_helper.rb
@@ -5,9 +5,9 @@ module UserFlyersHelper
   def UserFlyersHelper.getflyerpaths(userflyer, count, test)
     if (test)
       # this is a test response, don't send the created_at field
-      {"flyer_paths" => userflyer.flyer_paths.select("id, post1, post2, longitude1, longitude2, latitude1, latitude2, storms, stormed").limit(count).order("created_at DESC")}
+      {"flyer_paths" => userflyer.flyer_paths.select("id, post1, post2, longitude1, longitude2, latitude1, latitude2, storms, stormed, done").limit(count).order("created_at DESC")}
     else
-      {"flyer_paths" => userflyer.flyer_paths.select("id, created_at, post1, post2, longitude1, longitude2, latitude1, latitude2, storms, stormed").limit(count).order("created_at DESC")}
+      {"flyer_paths" => userflyer.flyer_paths.select("id, created_at, post1, post2, longitude1, longitude2, latitude1, latitude2, storms, stormed, done").limit(count).order("created_at DESC")}
     end
   end
 end
