bb0faff073a9a51983b007789a98b72a3295bcb9
Aaron Khoo <aaron.khoo@geolopigs.com>
2012-09-20 09:10:41 -0700

    More tests 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -120,7 +120,7 @@ class UsersController < ApplicationController
         @raw_friends = @user.fb_friends
         @friends_array = @raw_friends.split("|")
         @current_friend = User.where(:fbid => @friends_array).first
-        @current_user = User.find(@current_friend)
+        @current_user = User.find(2)
         @friends_list = @current_user.fb_friends
 
         test1 = "FB Friends 1:" + @friends_list
