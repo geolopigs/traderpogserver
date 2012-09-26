90d7fd3312bda9c0904773b83f60dda22ef7bd33
Aaron Khoo <aaron.khoo@geolopigs.com>
2012-09-26 14:40:23 -0700

    Return fb_name as part of user leaderboards

--- app/controllers/user_leaderboards_controller.rb
+++ app/controllers/user_leaderboards_controller.rb
@@ -5,7 +5,7 @@ class UserLeaderboardsController < ApplicationController
   include Logging
 
   # @param [Object] userid
-  def get_leaderboard_values_for_user(userid, fbid, member)
+  def get_leaderboard_values_for_user(userid, fbid, fbname, member)
     if ApplicationHelper.validate_key(request.headers["Validation-Key"])
       start_datetime = Time.utc(2000,"jan",1).beginning_of_week
     else
@@ -14,7 +14,7 @@ class UserLeaderboardsController < ApplicationController
     end
     boards = UserLeaderboard.where("user_id = ? and weekof = ?", userid, start_datetime.to_date).select("lbtype, lbvalue, weekof").as_json
     boards.each do |board|
-      board.merge!({ :fbid => fbid, :member => member })
+      board.merge!({ :fbid => fbid, :fb_name => fbname, :member => member })
     end
     boards
   end
@@ -29,7 +29,7 @@ class UserLeaderboardsController < ApplicationController
         if userid
           # First, get the list of leaderboard values for current user
           current_user = User.find(userid)
-          user_leaderboard << get_leaderboard_values_for_user(userid, current_user.fbid, current_user.member)
+          user_leaderboard << get_leaderboard_values_for_user(userid, current_user.fbid, current_user.fb_name, current_user.member)
 
           # Then, get leaderboard values for user's friends
           fbid_array = current_user.fb_friends
@@ -38,7 +38,7 @@ class UserLeaderboardsController < ApplicationController
             friends_list = User.where("fbid in (?)", fbid_array)
 
             friends_list.each do |friend|
-              user_leaderboard << get_leaderboard_values_for_user(friend.id, friend.fbid, friend.member)
+              user_leaderboard << get_leaderboard_values_for_user(friend.id, friend.fbid, friend.fb_name, friend.member)
             end
           end
 
