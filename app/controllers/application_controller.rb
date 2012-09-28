c47bba314313a2db809a75dec40dae4f9d04ee15
Aaron Khoo <aaron.khoo@geolopigs.com>
2012-09-28 16:21:20 -0700

    Adding basic auth to the html version of the site.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   skip_before_filter :verify_authenticity_token, :if => :json_request?
+  before_filter :authenticate, :unless => :json_request?
 
   protected
   def json_request?
     request.format.json?
+  end
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "pogadmin" && password == "0nlyP0gs"
+    end
   end
 end
